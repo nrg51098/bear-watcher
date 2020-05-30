4ade30cdb87a9e75d83798af3fd786b91c221afa
Nikhil Gaikwad <gaikwadrnikhil@gmail.com>
2020-05-30 14:05:55 -0500

    added bear river feature but not refractored the code yet

--- javascript/main.js
+++ javascript/main.js
@@ -1,43 +1,57 @@
 const bearDataArray=[];
-let id=0;
 
 
-const fetchData=(e)=>{
-const nameField=document.querySelector("#nameField");
-bearName = nameField.value;
-const imgUrlField=document.querySelector("#urlField");
-imgUrl = imgUrlField.value;
-const bearObj={
-    "id":id,
+
+const createNewBear=(e)=>{
+const bearName=document.querySelector("#nameField").value;
+const imgUrl=document.querySelector("#urlField").value;
+const newBearObj={
+    "id":`${Date.now()}`,
     "name":bearName,
-    "imgUrl":imgUrl
+    "imgUrl":imgUrl,
+    "fishCaught": 0
 }
+bearDataArray.unshift(newBearObj);
+createBearCards(bearDataArray);
+console.log(newBearObj)
 
-displayBearDetailsCard(bearObj);
 
-bearDataArray.push(bearObj);
-
-
-console.log(id);
-console.log(bearObj);
-// console.log(bearDataArray);
-
-id++;
-// printToDom(bearObjParam);
 e.preventDefault();
 
 }
 
-const displayBearDetailsCard=(bearObjParam)=>{
-    let domString = "";     
+const createBearCards=(bearDataArrayParam)=>{
+    let domString = "<div class='row'>";    
+    for (let i = 0; i < bearDataArrayParam.length; i++) {  
+        const bear=bearDataArrayParam[i];
+    
         domString += `
-            <div class="d-flex">
-            <div class="card center">   
-            <img id="detailImg" src="${bearObjParam.imgUrl}">   
-            <h1 id="headerId">Name: ${bearObjParam.name}</h1>
+        <div class="col-6 mb-4">
+        <div id="${bear.id}" class="card text-center dino-card">
+          <img class="card-img-top" src="${bear.imgUrl}" alt="Picture of the ${bear.name}">
+          <div class="card-body">
+          <div class="card-title">            
+              <h5>${bear.name}</h5>                                  
+          </div>
+          </div>
+          <div class="card-footer">
+            <div class="row">
+            <div class="col-6">
+            <h6 class="">Fish Caught: ${bear.fishCaught}</h6>            </div>
+            <div class="col-6">
+              <button type="button" class="m-auto btn btn-outline-warning catchButton"><i class="fab fa-tripadvisor"></i></button>
             </div>
             </div>
+          </div>          
+        </div>
+      </div>
         `; 
+    } 
+
+    const form = document.querySelector("#collapseOne");
+    form.classList.remove("show");
+
+    document.querySelector("#bear-form").reset(); 
    
     const bearData= document.querySelector("#bearCardsContainer");
     bearData.innerHTML= domString;
@@ -65,14 +79,14 @@ const displayBearDetailsCard=(bearObjParam)=>{
 //https://media.gettyimages.com/photos/brown-bear-picture-id163639080?s=612x612
 
 
-const getBearData =()=>{
-const submit = document.querySelector('button[type="submit"]');
-submit.addEventListener("click", fetchData)
-}
+
+
+
 
 
 const init =()=>{
-    getBearData();
+    const submit = document.querySelector('button[type="submit"]');
+    submit.addEventListener("click", createNewBear)
 }
 
 
